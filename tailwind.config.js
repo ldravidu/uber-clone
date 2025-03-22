ded33824c1ba0defd4aeacd0a633fb6899f59253
ldravidu-wsl <ldravidu@gmail.com>
2025-03-22 14:20:48 +0530

    initialize project with Expo, TypeScript, and NativeWind setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
