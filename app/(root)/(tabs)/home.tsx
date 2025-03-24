7eee98b5cc6cc605e5493517f5a0c1b9c43c37ed
ldravidu-wsl <ldravidu@gmail.com>
2025-03-24 10:46:15 +0530

    Add GoogleTextInput component, enhance HomeScreen with loading state and user greeting, and improve RideCard layout with additional ride details

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,7 +1,16 @@
+import GoogleTextInput from "@/components/GoogleTextInput";
 import RideCard from "@/components/RideCard";
+import { icons, images } from "@/constants";
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
-import { FlatList, Text } from "react-native";
+import {
+  FlatList,
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const recentRides = [
@@ -139,12 +148,73 @@ const recentRides = [
 
 export default function HomeScreen() {
   const { user } = useUser();
+  const loading = true;
+
+  const handleSignOut = () => {
+    // signOut();
+  };
+  const handleDestinationPress = () => {
+    // signOut();
+  };
 
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
         data={recentRides?.slice(0, 5)}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center h-full">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  className="w-40 h-40"
+                  alt="No recent rides found"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm text-neutral-400">
+                  No recent rides found
+                </Text>
+              </>
+            ) : (
+              <ActivityIndicator size="large" />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-2xl font-JakartaBold">
+                Welcome,{" "}
+                {user?.firstName ||
+                  user?.emailAddresses[0].emailAddress.split("@")[0]}
+                !
+              </Text>
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className="justify-center items-center w10 h-10 rounded-full bg-white"
+              >
+                <Image source={icons.out} className="w-5 h-5" />
+              </TouchableOpacity>
+            </View>
+
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle="bg-white shadow-md shadow-neutral-300"
+              handlePress={handleDestinationPress}
+            />
+
+            <>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Your Current Location
+              </Text>
+              <View className="flex flex-row items-center bg-transparent h-[300px]"></View>
+            </>
+          </>
+        )}
       />
     </SafeAreaView>
   );
